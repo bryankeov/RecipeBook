06b97f4973f31a48d8cd22034f5bb1cf0d6fb1d2
Bryan <suprnewb@gmail.com>
2023-03-20 17:24:25 +1100

    Add firebase

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5BEHtZ8z51hLqmuikqk3pvKM7t-Ivlk0",
+  authDomain: "recipebook-20e63.firebaseapp.com",
+  projectId: "recipebook-20e63",
+  storageBucket: "recipebook-20e63.appspot.com",
+  messagingSenderId: "648696269075",
+  appId: "1:648696269075:web:828056062216ab414632e6",
+  measurementId: "G-RD9TMHVY31",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
